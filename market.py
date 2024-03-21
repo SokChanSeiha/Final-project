c543d29d091404372c54c0506b87c1b34ea687c8
Leader <sokchanseiha@gmail.com>
2024-03-21 18:41:28 +0700

    feature 2 : add option 4

--- market.py
+++ market.py
@@ -37,7 +37,6 @@ while True:
         else:
             print(f"{pro_name} not found in inventory.")
     # issue3 : Create option to quit
-    # Tra
     elif user_input == "4":
         print("Quitting the program. Goodbye!")
         break
